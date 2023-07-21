014ac701f94781eab353fec6834cb9fcc8271be1
Wok <woctezuma@users.noreply.github.com>
2023-07-21 23:48:20 +0200

    Add utils for loading/saving JSON files

---
+++ src/json_utils.py
@@ -0,0 +1,24 @@
+import json
+from pathlib import Path
+
+
+def load_json(fname):
+    with Path(fname).open(encoding='utf8') as f:
+        data = json.load(f)
+    return data
+
+
+def load_json_failsafe(fname):
+    try:
+        data = load_json(fname)
+    except FileNotFoundError:
+        data = {}
+    return data
+
+
+def save_json(data, fname, prettify=False, indent=4):
+    with Path(fname).open('w', encoding='utf8') as f:
+        if prettify:
+            json.dump(data, f, indent=indent)
+        else:
+            json.dump(data, f)
