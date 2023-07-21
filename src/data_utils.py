57e58bf63b62c3eac72fccbf0aa0d17d4ceff0c4
Wok <woctezuma@users.noreply.github.com>
2023-07-21 23:49:28 +0200

    Add data filenames as constants

---
+++ src/data_utils.py
@@ -0,0 +1,5 @@
+DATA_FOLDER_NAME = "data"
+SANDBOX_FNAME = f"{DATA_FOLDER_NAME}/page_mappings.json"
+POLL_DATA_FNAME = f"{DATA_FOLDER_NAME}/poll_data.json"
+POLL_SUMMARY_FNAME = f"{DATA_FOLDER_NAME}/poll_summary.json"
+POLL_DICTIONARY_FNAME = f"{DATA_FOLDER_NAME}/poll_dictionary.json"
