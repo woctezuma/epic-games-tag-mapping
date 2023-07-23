3cdad8dc432a22244e0d8c7dcbfa57e36f9aef76
Wok <woctezuma@users.noreply.github.com>
2023-07-23 14:23:05 +0200

    Save tag names for later use in Colab

--- src/tag_utils.py
+++ src/tag_utils.py
@@ -1,3 +1,7 @@
+from src.data_utils import TAG_LIST_FNAME
+from src.json_utils import save_json
+
+
 def sort_tags(tag_dictionary, verbose=True):
     tags = sorted(set(tag_dictionary), key=int)
     tag_names = [tag_dictionary[tag]["resultTitle"] for tag in tags]
@@ -5,5 +9,7 @@ def sort_tags(tag_dictionary, verbose=True):
     if verbose:
         for tag_index, tag_title in zip(tags, tag_names, strict=True):
             print(f'- {tag_index.rjust(2)} -> {tag_title}')
+
+    save_json(tag_names, TAG_LIST_FNAME, prettify=True)
 
     return tags, tag_names
